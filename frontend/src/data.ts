0524b1977cbbc951f09f949d3f8a31a3831ef02c
Dev-musab <Dev.tatatech@gmail.com>
2023-01-24 18:54:37 +0400

    Second commit

---
+++ frontend/src/data.ts
@@ -0,0 +1,236 @@
+import {Food} from './app/shared/models/Food';
+import { Tag } from './app/shared/models/Tag';
+
+export const sample_foods: Food[] = [
+
+  {
+    id:'1',
+    name:'Kitfo/ክትፎ',
+    price:45,
+    favorite:true,
+    stars:3.5,
+    imageUrl:'/assets/Kitfo.jpg',
+    origins:['Minced meat cooked at your liking doused in Traditional clarified butter and spices'],
+    cookTime:'10-20',
+    tags:['Sara Specials'],
+  },
+
+  {
+    id:'2',
+    name:'Shiro/ሽሮ',
+    price:25,
+    cookTime:'15-20',
+    favorite:false,
+    origins:['Traditional stew that consists of chickpea flour, berbere powder, garlic or onion powder'],
+    stars:3.3,
+    imageUrl:'assets/Shiro.jpg',
+    tags:['Sara Specials']
+  },
+  {
+    id:'3',
+    name:'Tibs/ጥብስ',
+    price:50,
+    cookTime:'15-20',
+    favorite:true,
+    origins:['Sautéed beef or mutton cooked with Ethiopian vegetables and spices'],
+    stars:3.1,
+    imageUrl:'/assets/Tibs.jpg',
+    tags:['Sara Specials']
+  },
+  {
+    id:'4',
+    name:'Wonder Bread/ግሩም ድፎ ዳቦ',
+    price:40,
+    cookTime:'5-10',
+    favorite:false,
+    origins:['Classic Ethiopian bread consisting of staple Ethiopian herbs and spices'],
+    stars:3.3,
+    imageUrl:'/assets/Wonder Bread.jpg',
+    tags:['Sara Specials']
+  },
+  {
+    id:'5',
+    name:'Beef or Mutton Kikil/ቅቅል',
+    price:30,
+    cookTime:'15-20',
+    favorite:false,
+    origins:['Mutton simmered in butter, vegetables and spices until it is fall of the bone and served with its own broth usually mixed with pieces of injera'],
+    stars:3.3,
+    imageUrl:'/assets/Beef or Mutton Kikil.jpg',
+    tags:['Break Fast']
+  },
+  {
+    id:'6',
+    name:'Red Lentil Wot/ምስር ክክ',
+    price:25,
+    cookTime:'10-20',
+    favorite:true,
+    origins:['A stew of split red lentils cooked with an array of vegetables and spices usually onions, garlic, and berbere powder'],
+    stars:3.3,
+    imageUrl:'/assets/Red Lentil Wot.jpg',
+    tags:['Break Fast']
+  },
+  {
+    id:'7',
+    name:'Telba Fitfit/ተልባ ፍትፍት',
+    price:30,
+    cookTime:'5-15',
+    favorite:false,
+    origins:['Ground Flaxseed assorted with an array of Ethiopian vegetables and spices it usually consists of chilies, onions, garlic, and bits of injera soaked in a vegetable broth'],
+    stars:3.8,
+    imageUrl:'/assets/Telba Fitfit.jpg',
+    tags:['Break Fast']
+  },
+  {
+    id:'8',
+    name:'Gored Gored/ጎረድ ጎረድ',
+    price:45,
+    cookTime:'15-20',
+    favorite:true,
+    origins:['Meat sliced into small cubes and doused in traditional clarified butter it is usually served raw or rare'],
+    stars:3.1,
+    imageUrl:'/assets/Gored Gored.jpg',
+    tags:['Break Fast']
+  },
+  {
+    id:'9',
+    name:'Gomen Besiga/ጎመን በስጋ',
+    price:35,
+    cookTime:'10-20',
+    favorite:false,
+    origins:['Gomen Besiga/ጎመን በስጋ Collard greens sautéed with beef'],
+    stars:3.5,
+    imageUrl:'/assets/Gomen Besiga.jpg',
+    tags:['Break Fast']
+  },
+  {
+    id:'10',
+    name:'Key Yesiga Wot/ቅይ ወጥ',
+    price:30,
+    cookTime:'5-15',
+    favorite:false,
+    origins:['A classic Ethiopian stew cooked with onion, garlic, berbere powder and mutton'],
+    stars:3.7,
+    imageUrl:'/assets/Key Yesiga Wot.jpg',
+    tags:['Lunch']
+  },
+  {
+    id:'11',
+    name:'Shiro Bekibe/ሽሮ በቅቤ',
+    price:25,
+    cookTime:'15-20',
+    favorite:true,
+    origins:['Traditional stew that consists of chickpea flour, berbere powder, garlic or onion powder cooked with traditional Ethiopian clarified butter'],
+    stars:3.3,
+    imageUrl:'/assets/Shiro Bekibe.jpg',
+    tags:['Lunch']
+  },
+  {
+    id:'12',
+    name:'Alicha Yesiga Wot/አልጫ ወጥ',
+    price:30,
+    cookTime:'10-20',
+    favorite:false,
+    origins:['A classic Ethiopian stew made with beef or mutton cooked with vegetables usually onions'],
+    stars:3.3,
+    imageUrl:'/assets/Alicha Yesiga Wot.jpg',
+    tags:['Lunch']
+  },
+  {
+    id:'13',
+    name:'Agelgil/አገልግል',
+    price:75,
+    cookTime:'15-20',
+    favorite:true,
+    origins:['Traditional Ethiopian dish consisting of a variety of Ethiopian  non-veg dishes such as Key yesiga wot, Gomen besiga, shiro bekibe, Alicha yesiga wot, etc…'],
+    stars:3.9,
+    imageUrl:'/assets/Agelgil.jpg',
+    tags:['Lunch']
+  },
+  {
+    id:'14',
+    name:'Beyaynetu/በየአይነቱ',
+    price:35,
+    cookTime:'10-15',
+    favorite:false,
+    origins:['Traditional Ethiopian dish consisting of a variety of veg Ethiopian dishes such as Ater kik wot, collard greens, shiro wot, potato wot, Foselia, Red and brown Lentils et cetera'],
+    stars:3.3,
+    imageUrl:'/assets/Beyaynetu.jpg',
+    tags:['Lunch']
+  },
+  {
+    id:'15',
+    name:'Kinche/ቂንጬ',
+    price:25,
+    cookTime:'15-20',
+    favorite:false,
+    origins:['A traditional Ethiopian breakfast dish made from cracked wheat or oats it is cooked with butter or olive oil and it is served in a bowl much like quinoa'],
+    stars:3.5,
+    imageUrl:'/assets/Kinche.jpg',
+    tags:['Lunch']
+  },
+  {
+    id:'16',
+    name:'Spaghetti with Vegetables/ፓስታ ባአትክልት',
+    price:25,
+    cookTime:'15-20',
+    favorite:true,
+    origins:['Spaghetti with Vegetables/ፓስታ ባአትክልት Classic spaghetti dish cooked with a variety fried vegetables, the vegies usually consist of cabbage, onion, and carrots'],
+    stars:3.3,
+    imageUrl:'/assets/Spaghetti with Vegetables.jpg',
+    tags:['Dinner']
+  },
+  {
+    id:'17',
+    name:'Fasting Firfir (veg or vegan)/የጾም ፍርፍር',
+    price:25,
+    cookTime:'15-25',
+    favorite:true,
+    origins:['Injera bits doused in a vegetable stew consisting of a variety of vegetables and spices such as onions, garlic, tomato, and berbere powder'],
+    stars:3.8,
+    imageUrl:'/assets/Fasting Firfir (veg or vegan).jpg',
+    tags:['Dinner']
+  },
+  {
+    id:'18',
+    name:'Brown Lentils Wot/ድፍን ምስር',
+    price:40,
+    cookTime:'15-25',
+    favorite:false,
+    origins:['A stew of split red lentils cooked with an array of vegetables and spices usually onions, garlic, and berbere powder'],
+    stars:3.1,
+    imageUrl:'/assets/Brown Lentils Wot.jpg',
+    tags:['Dinner']
+  },
+  {
+    id:'19',
+    name:'Firfir Besiga/ፍርፍር በስጋ',
+    price:50,
+    cookTime:'10-15',
+    favorite:true,
+    origins:['Injera bits doused in a beef or mutton stew consisting of a variety of vegetables and spices such as onions, garlic, tomato, and berbere powder (also includes a boiled egg in the center of the meal'],
+    stars:3.8,
+    imageUrl:'/assets/Firfir Besiga.jpg',
+    tags:['Dinner']
+  },
+  {
+    id:'20',
+    name:'Spaghetti with Meat sauce/ፓስታ በስጋ',
+    price:45,
+    cookTime:'15-25',
+    favorite:false,
+    origins:['Classic spaghetti dish cooked and served with a classic Bolognese meat sauce'],
+    stars:3.3,
+    imageUrl:'/assets/Spaghetti with Meat sauce.jpg',
+    tags:['Dinner']
+  },
+]
+
+
+export const sample_tags:Tag[] = [
+  {name: 'All',count:20},
+      {name: 'Sara Specials',count:4},
+      {name: 'Break Fast',count:5},
+      {name: 'Lunch',count:6},
+      {name: 'Dinner',count:5},
+]
